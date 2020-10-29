84c0fbef664a1144f6fd8320fcd625a71d409d68
Nick Papazetis <papazetis@gmail.com>
2020-10-29 21:15:04 +0000

    Update header.php

--- header.php
+++ header.php
@@ -20,42 +20,37 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+
     <?php wp_body_open(); ?>
 
-    <header id="masthead" class="site-header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 p-0">
+    <div id="page" class="site">
+        <header id="masthead" class="site-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 p-0">
 
 
-                        <div id="site-header">
-                            <div class="header-image">
-                                <?php if (is_front_page()): ?>
-                                    <img src="<?php echo get_header_image() ?>">
-                                <?php endif; ?>
-                            </div>
+                            <div id="site-header">
 
-                            <div class="logo">
-                                <a href="<?php echo home_url(); ?>">
+                                <div class="logo">
                                     <?php echo get_custom_logo(); ?>
-                                </a>
+                                </div>
+
+
+                                <nav id="site-navigation" class="main-navigation">                                        
+                                    <?php
+                                    wp_nav_menu(array('theme_location' => 'my-custom-menu', 'menu_id' => 'primary-menu', 'container_class' => 'custom-menu-class'));
+                                    ?>
+                                </nav><!-- #site-navigation -->
+
+
+
+
                             </div>
 
-
-                            <nav id="site-navigation" class="main-navigation">
-                                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-                                    <?php esc_html_e('Primary Menu', 'dos'); ?></button>
-                                <?php
-                                wp_nav_menu(array('theme_location' => 'my-custom-menu', 'menu_id' => 'primary-menu', 'container_class' => 'custom-menu-class'));
-                                ?>
-                            </nav><!-- #site-navigation -->
-
-
-                        </div>
-
+                    </div>
                 </div>
             </div>
-        </div>
-    </header><!-- #masthead -->
-<!--    <div id="content" class="site-content">-->
+        </header><!-- #masthead -->
+        
+        <div id="content" class="site-content">

a292854784557d6d57fa809f47ae0fecb60e126b
alexander iwsifidis <alexis_iwsifidis@hotmail.com>
2020-11-01 15:53:06 +0200

    these are template-our-place.php

---
+++ template-our-place.php
@@ -0,0 +1,103 @@
+<?php
+
+
+/* Template Name: Our Place */
+
+
+get_header();
+?>
+
+<section class="Our-Place" style="background-image: url('../wp-content/themes/dos/images/header_photo.jpg');"></section>
+<section class="Our-Place-wrapper" style="background-image: url('../wp-content/themes/dos/images/bg_wall.jpg');">
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1>Our story</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="flex-our">
+
+
+                    <p class="our-lighter">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
+                        of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure
+                        Latin
+                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
+                        1.10.32
+                        and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero,
+                        written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+                        Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a
+                        line in
+                        section 1.10.32.
+                    </p>
+
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="flex-our">
+                    <p class="our-lighter">
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
+                        of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure
+                        Latin
+                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
+                        1.10.32
+                        and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero,
+                        written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+                        Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a
+                        line in
+                        section 1.10.32.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="Our-Place-Slider">
+<div class="container-fluid p-0">
+    <div class="row no-gutters">
+        <div class="col-lg-12 p-0">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="http://localhost/Mariz/wp-content/themes/dos/images/Photo_slider.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+        </div>
+    </div>
+</div>
+</section>
+
+

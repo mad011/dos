5deaf4dc5cf964f84e335106fc8d8b2d3cb17a96
Nick Papazetis <papazetis@gmail.com>
2020-11-02 21:17:13 +0000

    Remove testimonial.php from front-page.php

--- front-page.php
+++ front-page.php
@@ -2,13 +2,10 @@
 
 get_header();
 
-
 get_template_part('/template-parts/homepage/header');
 get_template_part('/template-parts/homepage/section');
 get_template_part('/template-parts/homepage/query');
 get_template_part('/template-parts/homepage/slider');
 get_template_part('/template-parts/homepage/fiesta');
-get_template_part('/template-parts/homepage/testimonial');
-
 
 get_footer();
